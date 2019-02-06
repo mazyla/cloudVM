8b88cdf3882ddc1315807b12f3975587f7d7eeb6
Mario Zyla <mazyla@wpi.edu>
2019-02-05 23:11:56 -0500

    Added Handle query

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import os
 from flask import (Flask, request)
 import face_recognition as fc
 
-
 app = Flask(__name__)
 print("__name__ is", __name__)
 
@@ -24,6 +23,7 @@ names = ["Rolando", "Mario", "Mario"]
 
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+
 @app.route('/upload', methods=['POST'])
 def upload():
     if request.method == 'POST':
@@ -31,29 +31,35 @@ def upload():
         print(file)
         extension = os.path.splitext(file.filename)[1]
         name_to_save = "unknown_latest_image" + extension
-        #name_to_save = file.filename
+        # name_to_save = file.filename
         file.save(os.path.join("./uploads/", name_to_save))
-    
+
     return findFace(name_to_save)
+
 
 def findFace(name_to_save):
     print(name_to_save)
-    unknown_image = fc.load_image_file("./uploads/"+name_to_save)
+    unknown_image = fc.load_image_file("./uploads/" + name_to_save)
     unknown_image_encoding = fc.face_encodings(unknown_image)
-    if (len(unknown_image_encoding)==0):
+    if (len(unknown_image_encoding) == 0):
         print("couldn't find face")
         return "No Face"
     print("Size: ", len(unknown_image_encoding))
     results = fc.compare_faces(face_encodings, unknown_image_encoding[0], tolerance=0.6)
     index = 0
     for result in results:
-        if result==True:
+        if result == True:
             return names[index]
-        index+=1
+        index += 1
     return "Unknown"
+
+@app.route('/sqlQuery', methods=['GET', 'POST'])
+def sqlQuery():
+    if request.method == 'POST':
+        file = request.files['queryString']
+        print(file)
 
 
 if __name__ == '__main__':
-    #app.run(host="13.68.140.235", port="5007")
+    # app.run(host="13.68.140.235", port="5007")
     app.run(host='0.0.0.0', debug=True, threaded=True)
-
